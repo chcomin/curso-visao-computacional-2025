230f78bc161e91cb837435b08c5484a4b2fc7626
Cesar Henrique Comin <chcomin@gmail.com>
2025-05-10 18:40:35 -0300

    Small changes for next class

--- M03_pipeline_de_treinamento/dataset.py
+++ M03_pipeline_de_treinamento/dataset.py
@@ -7,6 +7,7 @@ from torchvision import datasets
 
 
 class Subset(Dataset):
+    """Cria um subconjunto de um dataset."""
 
     def __init__(self, ds, indices, transform=None):
         self.ds = ds
@@ -23,28 +24,39 @@ class Subset(Dataset):
 
     def __len__(self):
         return len(self.indices)
+    
+class Transform:
+    """Transforma as imagens de um dataset."""
 
-def transform_mnist(img, mean=33., std=76.5):
-    """Transforma uma imagem do MNIST. A média e desvio padrão utilizados na
-    normalização foram calculados previamente.
-    """
-    # Conversão PIL->numpy
-    img = np.array(img, dtype=np.float32)
-    # Conversão numpy->pytorch
-    img = torch.from_numpy(img)
-    # Normalização
-    img = (img-mean)/std
-    # Adição de um canal
-    img = img.reshape(1, img.shape[0], img.shape[1])
+    def __init__(self, mean, std):
 
-    return img
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, img):
+        """Transformação PIL->numpy->tensor->z-score."""
+
+        # Conversão PIL->numpy
+        img = np.array(img, dtype=np.float32)
+        # Conversão numpy->pytorch
+        img = torch.from_numpy(img)
+        # Normalização
+        img = (img-self.mean)/self.std
+        # Adição de um canal
+        img = img.reshape(1, img.shape[0], img.shape[1])
+
+        return img
 
 def load_mnist(root="../data", n=1000):
+    """Carrega o dataset MNIST e cria subconjuntos de treino e validação."""
+
+    MEAN = 33.0
+    STD = 76.5
 
     ds = datasets.MNIST(root, train=True, download=True)
     random.seed(42)
     indices = random.sample(range(len(ds)), k=2*n)
-    ds_train = Subset(ds, indices[:n], transform_mnist)
-    ds_valid = Subset(ds, indices[n:], transform_mnist)
+    ds_train = Subset(ds, indices[:n], Transform(MEAN, STD))
+    ds_valid = Subset(ds, indices[n:], Transform(MEAN, STD))
 
     return ds_train, ds_valid
